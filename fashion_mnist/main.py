c9fda920d4fd55beed017e5ef7bcd4e10166f5b3
John Naylor <jonaylor89@gmail.com>
2020-02-16 13:42:53 -0500

    Display images

--- fashion_mnist/main.py
+++ fashion_mnist/main.py
@@ -1,4 +1,5 @@
 
+from matplotlib import pyplot
 from keras.datasets import fashion_mnist 
 
 def main():
@@ -9,6 +10,17 @@ def main():
     # summarize the shape of the dataset
     print('Train', trainX.shape, trainy.shape)
     print('Test', testX.shape, testy.shape)
+
+    # plot images from the training dataset
+    for i in range(100):
+        # define subplot
+        pyplot.subplot(10, 10, 1 + i)
+        # turn off axis
+        pyplot.axis('off')
+        # plot raw pixel data
+        pyplot.imshow(trainX[i], cmap='gray_r')
+
+    pyplot.show()
 
 
 if __name__ == '__main__':
